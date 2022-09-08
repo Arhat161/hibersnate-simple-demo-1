ab621c5c328419489aa00b866c5f3ed766d8d367
Arhat161 <Arhat161@users.noreply.github.com>
2022-09-08 16:14:50 +0300

    add schema.sql

---
+++ schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Person (
+                        id int PRIMARY KEY,
+                        name varchar(100),
+                        age int
+);
+
+insert into person(id, name, age) values (1, 'Test Person', 20);
