7e5e200eb098e2e7460369c4a142d4fa20b71b8e
Arhat161 <Arhat161@users.noreply.github.com>
2022-09-08 16:13:08 +0300

    first working version

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,23 +10,29 @@ import org.hibernate.cfg.Configuration;
  */
 public class App {
     public static void main(String[] args) {
-        // First, create Configuration
+        // First, create Configuration (all connection properties in 'hibernate.properties' file
         Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
-
+        // Second, create sessionFactory
         SessionFactory sessionFactory = configuration.buildSessionFactory();
-
+        // Third, get session to wor from sessionFactory
         Session session = sessionFactory.getCurrentSession();
 
         try {
+            // open transaction
             session.beginTransaction();
 
-            Person person = session.get(Person.class, 1);
+            Person person = session.get(Person.class, 1); // SELECT * FROM Person WHERE id=1
+
             System.out.println(person.getName());
             System.out.println(person.getAge());
 
+            // close transaction
             session.getTransaction().commit();
+
         } finally {
+            // close sessionFactory
             sessionFactory.close();
+
         }
 
     }
